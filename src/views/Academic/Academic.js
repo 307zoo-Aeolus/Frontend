26b1c25bfd513d10074c20948cbb5a0d8b69a9a6
TannyXie <tannyxie@TannyXiedeMacBook.local>
2020-07-05 20:01:38 +0800

    changed the Academic page

--- src/views/Academic/Academic.js
+++ src/views/Academic/Academic.js
@@ -3,6 +3,7 @@ import React from "react";
 import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
+import Card from '@material-ui/core/Card';
 
 // @material-ui/icons
 
@@ -22,13 +23,206 @@ import ProductSection from "./Sections/ProductSection.js";
 import TeamSection from "./Sections/TeamSection.js";
 import WorkSection from "./Sections/WorkSection.js";
 
+import clsx from 'clsx';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 const dashboardRoutes = [];
 
 const useStyles = makeStyles(styles);
 
+const cardStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+const gridStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 export default function LandingPage(props) {
   const classes = useStyles();
   const { ...rest } = props;
+
+  const cardcls = cardStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  //grid
+  const gridcls = gridStyles();
+
+  const fakecard = ()=> {
+    return (
+      <Card className={cardcls.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={cardcls.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            This impressive paella is a perfect party dish and a fun meal to cook together with your
+            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={clsx(cardcls.expand, {
+              [cardcls.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+              minutes.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    )
+  }
+
+  const cardcontent = ()=>{
+    return (
+      <div>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="recipe" className={cardcls.avatar}>
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              This impressive paella is a perfect party dish and a fun meal to cook together with your
+              guests. Add 1 cup of frozen peas along with the mussels, if you like.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton
+              className={clsx(cardcls.expand, {
+                [cardcls.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method:</Typography>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                minutes.
+              </Typography>
+            </CardContent>
+          </Collapse>
+      </div>
+    )
+  }
+
+  const arrangement = ()=>{
+    return(
+      <div className={gridcls.root}>
+        <Grid container spacing={4}>
+          <Grid item xs={4}>
+            <Paper className={gridcls.paper}>
+              {cardcontent()}
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={gridcls.paper}>
+              {cardcontent()}
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={gridcls.paper}>
+              {cardcontent()}
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>)
+  };
+
   return (
     <div>
       <Header
@@ -42,41 +236,57 @@ export default function LandingPage(props) {
           color: "white",
         }}
         {...rest}
+        marginTop="10px"
       />
-      <Parallax filter image={require("assets/img/about-bg.jpg")}>
+      <Parallax filter image={require("assets/img/about-bg.jpg")}
+        marginTop="10px">
         <div className={classes.container}>
           <GridContainer>
-            <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+            <GridItem xs={12} sm={12} md={3}>
+              <h1 className={classes.title}>Academic</h1>
               <h4>
-                Every landing page needs a small description after the big bold
-                title, that{"'"}s why we added this text here. Add here all the
-                information that can make you or your product create the first
-                impression.
+              Get Info on Updated Online Forums & Labs’ Positions.
               </h4>
               <br />
-              <Button
+                 
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          {arrangement()}
+        </div>
+      </div>
+
+      
+      <Footer />
+    </div>
+  );
+}
+
+/**
+ * 
                 color="danger"
                 size="lg"
                 href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
                 target="_blank"
                 rel="noopener noreferrer"
-              >
+ */
+
+/**
+ * 
                 <i className="fas fa-play" />
                 Watch video
-              </Button>
-            </GridItem>
-          </GridContainer>
-        </div>
-      </Parallax>
-      <div className={classNames(classes.main, classes.mainRaised)}>
+ */
+
+/**
+ *       <div className={classNames(classes.main, classes.mainRaised)}>
         <div className={classes.container}>
           <ProductSection />
           <TeamSection />
           <WorkSection />
         </div>
       </div>
-      <Footer />
-    </div>
-  );
-}
+ */
